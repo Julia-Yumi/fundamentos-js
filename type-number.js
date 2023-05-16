4de7a58739736e7b231d7bf4653e227174373454
Julia Yumi <131373662+Julia-Yumi@users.noreply.github.com>
2023-05-16 11:34:12 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,21 @@
+// tipo number
+const meuNumero = 3;
+
+const primeiroNumero= 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log(operacaoMatematica);
+
+const operacaoMat = primeiroNumero * segundoNumero;
+console.log(operacaoMat);
+
+//ponto flutuante
+const numeroPontoFlutuante = 3.3525184752;
+const numeroPontoFlutuanteSemZero = .5;
+const divisao = numeroPontoFlutuante / numeroPontoFlutuanteSemZero;
+console.log(divisao);
+
+//NaN not a number 
+const alura = "Alura"
+console.log(alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá mundo!'
+const senha = "senhaSuperSegura123"
+const stringDeNumeros = "123456";
+const citacao = 'A Julia disso "oi!"';
+
+console.log(citacao);
+
+//template string ou template literal
+
+const nome = "Julia";
+const segNome = " Yumi";
+const nomeCompleto = nome + segNome;
+console.log(nomeCompleto);
